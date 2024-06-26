cf1854852fad987d53346d8512e105da97a7b85c
mucandev <mucandev@gmail.com>
2024-06-26 20:45:26 +0200

    etape 1 en cours

--- functions.php
+++ functions.php
@@ -3,7 +3,26 @@
  * @link https://developer.wordpress.org/themes/basics/theme-functions/
  */
 
- function motaphoto_add_admin_pages() {
+ //chargement des styles et des scripts
+function motaphoto_enqueue_styles() {
+    wp_enqueue_style('motaphoto-style', get_stylesheet_uri() );
+}
+add_action('wp_enqueue_scripts', 'motaphoto_enqueue_styles');
+
+// Ajout de la gestion de menu dans le dashboard  wordpress
+function register_custom_menu() {
+    register_nav_menus(array(
+        'menu_principal' => __('Menu principal', 'Photographe'),
+        'menu_secondaire' => __('Menu secondaire', 'Photographe'),
+    ));
+}
+add_action('init', 'register_custom_menu');
+
+
+
+
+
+function motaphoto_add_admin_pages() {
     add_menu_page(__('Paramètres du thème motaphoto', 'motaphoto'), __('motaphoto', 'motaphoto'), 'manage_options', 'motaphoto-settings', 'motaphoto_theme_settings', 'dashicons-admin-settings', 60);
 }
 
--- header.php
+++ header.php
@@ -16,7 +16,7 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>"/>
 	<meta name="viewport" content="width=device-width, initial-scale=1"/>
     <title><?php bloginfo('name') ?> - <?php bloginfo('description') ?></title>
-    <link rel="icon" type="image/vnd.icon" href="<?php echo get_theme_file_uri() . '/assets/images/motaphoto-icon.png'; ?>" >
+    <link rel="icon" type="image/vnd.icon" href="<?php echo get_template_directory_uri() . '/assets/images/motaphoto-icon.png'; ?>" >
 	<?php wp_head(); ?>
 </head>
 
